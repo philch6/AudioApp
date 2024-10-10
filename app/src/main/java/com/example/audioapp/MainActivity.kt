73ffb6fbba1224c9b8262843d387624fe9bbcc87
philch6 <philch@tds.net>
2024-10-09 21:02:21 -0500

    Added code for audio buttons Added mic permissions to manifest

--- app/src/main/java/com/example/audioapp/MainActivity.kt
+++ app/src/main/java/com/example/audioapp/MainActivity.kt
@@ -5,8 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.pm.PackageManager
+import android.widget.Button
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBtnRecord: Button
+    private lateinit var mBtnPlay: Button
+    private lateinit var mBtnStop: Button
+
+    private var audioFilePath: String? = null
+    private var isRecording = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mBtnRecord = findViewById(R.id.btnRecord)
+        mBtnPlay   = findViewById(R.id.btnPlay)
+        mBtnStop   = findViewById(R.id.btnStop)
+    }
+
+    private fun hasMicrophone(): Boolean {
+        val pmanager = this.packageManager
+        return pmanager.hasSystemFeature(
+            PackageManager.FEATURE_MICROPHONE)
+    }
+
+    private fun audioSetup() {
+        if (!hasMicrophone()) {
+            mBtnStop.isEnabled = false
+            mBtnPlay.isEnabled = false
+            mBtnRecord.isEnabled = false
+        } else {
+            mBtnPlay.isEnabled = false
+            mBtnStop.isEnabled = false
+        }
+        val audioFile = File(this.filesDir, "myaudio.3gp")
+        audioFilePath = audioFile.absolutePath
     }
 }
--- gitHelper.bat
+++ gitHelper.bat
@@ -1,10 +1,10 @@
 REM Git Helper. Intended to be ran from the new project's working folder
-git init
-git add .
-git commit -m "First Commit"
+REM git init
+REM git add .
+REM git commit -m "First Commit"
 REM
 REM Create the remote repo and get the specific Git Repo URL
 REM Run this command:
-REM git remote add origin "https://github.com/SharePerson/kaizen.git"
+REM git remote add origin "https://github.com/philch6/AudioApp.git"
 REM git push origin master
 pause
